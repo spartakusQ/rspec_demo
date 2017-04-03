8397e3731d2e1fe3fbb37760eefa93b79f1c479f
spartakusQ <spartakus1688@gmail.com>
2017-04-03 13:13:48 +0300

    fix to class

--- Shuffle/shuffle.rb
+++ Shuffle/shuffle.rb
@@ -1,7 +1,10 @@
-def shuffle(arr)
-  for i in arr do
-    arr.insert(rand(0..arr.length-1), arr.delete(i))
+class Shuffle
+  def shuffle(arr)
+    for i in arr do
+      arr.insert(rand(0..arr.length-1), arr.delete(i))
+    end
   end
 end
+
 
 puts shuffle([1, 4, 134, 42, 125, 21, 55]).inspect
